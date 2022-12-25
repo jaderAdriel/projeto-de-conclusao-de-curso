be864415c6fa4ce2fc23b5df034728ed92ea4b8f
jaderAdriel <jaderadriel7@gmail.com>
2022-12-25 17:51:56 -0300

    :fire: removing tracked files that are in .gitignore

--- code/monitoringPlatform/Users/views.py
+++ code/monitoringPlatform/Users/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
+from django.http import HttpResponseRedirect
+from django.shortcuts import redirect, render
+
 # Create your views here.
+def registrarUsuario(request):
+    if request.method == "POST":
+        ...
+    else: 
+        ...
+
+      
+
+    return render(request, 'registration/registrar.html')
