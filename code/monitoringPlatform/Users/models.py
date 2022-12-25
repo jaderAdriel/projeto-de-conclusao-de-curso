b00c847a776a0a638bbdabd1d0b30a7314eb5e35
jaderAdriel <jaderadriel7@gmail.com>
2022-12-25 15:36:54 -0300

    :wrench: code: create custom user model

--- code/monitoringPlatform/Users/models.py
+++ code/monitoringPlatform/Users/models.py
@@ -1,3 +1,57 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.base_user import BaseUserManager
+
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError('Users require an email field')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(email, password, **extra_fields)
+
+
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(('email address'), unique=True)
+    name = models.CharField(
+        max_length=250,
+        blank=False,
+        null=False
+    )
+
+    cpf = models.CharField(
+        max_length=14,
+        blank=False,
+        null=False
+    )
+
+    objects = UserManager()
+
+    # Change the field username for email
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
