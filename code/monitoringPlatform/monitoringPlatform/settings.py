7767bc352e951e0fee3bb3fe2d5899a88f76117b
jaderAdriel <jaderadriel7@gmail.com>
2022-12-26 11:19:28 -0300

    :construction: chore: auth paths

--- code/monitoringPlatform/monitoringPlatform/settings.py
+++ code/monitoringPlatform/monitoringPlatform/settings.py
@@ -121,5 +121,6 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [ str(BASE_DIR) + "/static"]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
+LOGIN_REDIRECT_URL = "/"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
