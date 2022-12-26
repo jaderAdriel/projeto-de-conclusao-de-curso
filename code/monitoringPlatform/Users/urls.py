b3ce8b28e9b5470dc703fa57031f1d3768c7afd8
jaderAdriel <jaderadriel7@gmail.com>
2022-12-26 09:47:08 -0300

    :wrench: add url configurations

--- code/monitoringPlatform/Users/urls.py
+++ code/monitoringPlatform/Users/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # http://127.0.0.1:8000/accounts/register/
-    path('registrar/', views.registrarUsuario, name='registrar'),
+    # http://127.0.0.1:8000/accounts/signup/
+    path('signup/', views.signup, name='signup'),
+    path('login/', views.entrar, name='login'),
 ]
