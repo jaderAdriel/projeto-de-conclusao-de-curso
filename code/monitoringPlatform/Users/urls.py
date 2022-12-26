60834401ec6ff19a782d7512c2ee1156399253b6
jaderAdriel <jaderadriel7@gmail.com>
2022-12-26 10:55:02 -0300

    :construction: chore: creating urls

--- code/monitoringPlatform/Users/urls.py
+++ code/monitoringPlatform/Users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
     # http://127.0.0.1:8000/accounts/signup/
     path('signup/', views.signup, name='signup'),
-    path('login/', views.entrar, name='login'),
+    path( 'login/', views.login, name='login')
 ]
