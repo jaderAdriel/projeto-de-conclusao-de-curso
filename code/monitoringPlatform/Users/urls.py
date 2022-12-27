c2f0784e9c5c5d82c29d48c2806ad511d846f1f2
jaderAdriel <jaderadriel7@gmail.com>
2022-12-27 00:05:44 -0300

    :wrench: chore: configuration of urls to register users

--- code/monitoringPlatform/Users/urls.py
+++ code/monitoringPlatform/Users/urls.py
@@ -5,5 +5,10 @@ from . import views
 urlpatterns = [
     # http://127.0.0.1:8000/users/signup/
     path('signup/', views.signup, name='signup'),
+
+    path('register-client/', views.registerClient, name='registerClient'),
+    
+    path('register-professional/', views.registerProfessional, name='register-professional'),
     # http://127.0.0.1:8000/users/login/
+    path('login/', views.login, name='login'),
 ]
