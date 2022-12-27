002e8dac06348bc63002b9b241f3b86e12b863d3
jaderAdriel <jaderadriel7@gmail.com>
2022-12-27 00:03:59 -0300

    :construction: feat: add register views for users

--- code/monitoringPlatform/Users/views.py
+++ code/monitoringPlatform/Users/views.py
@@ -1,19 +1,73 @@
 
 from django.shortcuts import render
 
-from django.http import HttpResponse, JsonResponse
+from django.http import JsonResponse
 
 from django.shortcuts import redirect, render
 
-from Users.forms import UserLoginForm
+from Users.forms import RegisterAdminForm, RegisterClientForm, RegisterProfessionalForm
 
 # Create your views here.
-def signup(request):
-    if request.method == "POST":
-        ...
-        return JsonResponse({'status': '200'})
 
-    else: 
-        ...
+def login( request ):
+    ...
+
+#--------------------------------------------------------------------
+
+def signup( request ):
 
     return render(request, 'registration/signup.html')
+
+#--------------------------------------------------------------------
+
+def registerClient( request ):
+
+    if request.method == "POST":
+
+        form = RegisterClientForm( request.POST )
+
+        if form.is_valid():
+            form.save()
+
+
+    context = {
+            'form' : RegisterClientForm
+        }
+    
+    return render(request, 'registration/register-client.html', context)
+
+#--------------------------------------------------------------------
+
+def registerAdmin( request ):
+
+    if request.method == "POST":
+
+        form = RegisterAdminForm( request.POST )
+
+        if form.is_valid():
+            form.save()
+
+
+    context = {
+            'form' : RegisterAdminForm
+        }
+    
+    return render(request, 'registration/register-admin.html', context)
+
+#--------------------------------------------------------------------
+
+def registerProfessional( request ):
+    
+    if request.method == "POST":
+
+        form = RegisterProfessionalForm( request.POST )
+
+        if form.is_valid():
+            form.save()
+
+
+    context = {
+            'form' : RegisterProfessionalForm
+        }
+    
+    return render(request, 'registration/register-professional.html', context)
