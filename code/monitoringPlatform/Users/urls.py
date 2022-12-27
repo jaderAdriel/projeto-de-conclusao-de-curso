a6ae572fe14b36247412816514e8af7930327f8b
jaderAdriel <jaderadriel7@gmail.com>
2022-12-27 18:53:18 -0300

    :wrench: chore: configurate register users type url

--- code/monitoringPlatform/Users/urls.py
+++ code/monitoringPlatform/Users/urls.py
@@ -3,12 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # http://127.0.0.1:8000/users/signup/
-    path('signup/', views.signup, name='signup'),
-
+    # http://127.0.0.1:8000/users/register-client/
+    path('register-admin/', views.signup, name='signup'),
+    # http://127.0.0.1:8000/users/register-client/
     path('register-client/', views.registerClient, name='registerClient'),
-    
-    path('register-professional/', views.registerProfessional, name='register-professional'),
-    # http://127.0.0.1:8000/users/login/
-    path('login/', views.login, name='login'),
+    # http://127.0.0.1:8000/users/register-professional/
+    path('register-professional/', views.registerProfessional, name='registerProfessional'),
 ]
